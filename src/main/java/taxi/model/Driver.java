05c478393101e20cfb301b2001f3832dd2f61fea
Daniel Kuruch <kuruch1004@gmail.com>
2021-07-28 20:00:04 +0300

    created model, configured connection to DB,created custom exception and annotations, custom mechanism IoC

---
+++ src/main/java/taxi/model/Driver.java
@@ -0,0 +1,92 @@
+package taxi.model;
+
+import java.util.Objects;
+import java.util.StringJoiner;
+
+public class Driver {
+    private Long id;
+    private String name;
+    private String licenseNumber;
+    private String login;
+    private String password;
+
+    public Driver(String name, String licenseNumber) {
+        this.name = name;
+        this.licenseNumber = licenseNumber;
+    }
+
+    public Driver(String name, String licenceNumber, String login, String password) {
+        this(name, licenceNumber);
+        this.login = login;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Driver driver = (Driver) o;
+        return Objects.equals(id, driver.id)
+                && Objects.equals(name, driver.name)
+                && Objects.equals(licenseNumber, driver.licenseNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, licenseNumber);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Driver.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("name='" + name + "'")
+                .add("licenseNumber='" + licenseNumber + "'")
+                .add("login='" + login + "'")
+                .toString();
+    }
+}
